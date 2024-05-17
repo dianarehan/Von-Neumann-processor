54ad0300c6ab3d2792865f1d85b37018f7d25e6b
diana <daiana.rehan.dr@gmail.com>
2024-05-17 21:58:43 +0300

    final

---
+++ CA thing/build/CMakeFiles/TestingProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestingProject.dir/testing.c.o"
+  "CMakeFiles/TestingProject.dir/testing.c.o.d"
+  "TestingProject"
+  "TestingProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TestingProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
